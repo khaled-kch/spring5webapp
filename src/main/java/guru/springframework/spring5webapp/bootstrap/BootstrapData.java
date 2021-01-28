d0973a4d66a52460649449820ef53f10c92afda9
kchayeb <kchayeb>
2021-01-28 23:08:23 +0100

    add boostrap data

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootstrapData.java
@@ -1,2 +1,64 @@
-package guru.springframework.spring5webapp.bootstrap;public class BootstrapData {
+package guru.springframework.spring5webapp.bootstrap;
+
+import guru.springframework.spring5webapp.domain.Author;
+import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootstrapData implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
+
+    public BootstrapData(AuthorRepository authorRepository,
+                         BookRepository bookRepository,
+                         PublisherRepository publisherRepository
+    ) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Started in Boostrap");
+
+        Publisher publisher = new Publisher();
+        publisher.setName("KCH Publishing");
+        publisher.setCity("Nantes");
+        publisher.setState("France");
+        publisherRepository.save(publisher);
+
+        Author bertrand = new Author("Bertrand", "Russell");
+        Book scienceAndReligion = new Book("Science and Religion", "9782070325177");
+        bertrand.getBooks().add(scienceAndReligion);
+        scienceAndReligion.getAuthors().add(bertrand);
+        scienceAndReligion.setPublisher(publisher);
+        publisher.getBooks().add(scienceAndReligion);
+
+        authorRepository.save(bertrand);
+        bookRepository.save(scienceAndReligion);
+        publisherRepository.save(publisher);
+
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Development", "9782070325188");
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+        noEJB.setPublisher(publisher);
+        publisher.getBooks().add(noEJB);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+        publisherRepository.save(publisher);
+
+        System.out.println("Number of Books: " + bookRepository.count());
+        System.out.println("Number of Authors: " + authorRepository.count());
+        System.out.println("Publisher Number of Books: " + publisher.getBooks().size());
+    }
 }
